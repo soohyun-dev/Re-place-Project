c662396c30bff5d74e3fb574ba298bdd1b1ab499
soohyun <bmy1320@gmail.com>
2022-12-15 23:42:50 +0900

    feat(Main): api 호출 결과 화면 출력 확인
    1. handleAPI 기능 테스트

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import { tmpAPI } from "../API/api";
 
 const Main = () => {
-  console.log(tmpAPI());
+  const [test, setTest] = useState("");
+  const handleAPI = async () => {
+    const result = await tmpAPI();
+    console.log(result.response.body.items.item[0]["addr1"]);
+    setTest(result.response.body.items.item[0]["addr1"]);
+  };
+
+  useEffect(() => {
+    handleAPI();
+  }, []);
+
   return (
-    <div>
-      <h1>123</h1>
-    </div>
+    <>
+      <div>
+        <h1>{test}</h1>
+      </div>
+    </>
   );
 };
 
