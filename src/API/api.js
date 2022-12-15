9a9232826c1086845f315694d03a3dc2191b2b18
soohyun <bmy1320@gmail.com>
2022-12-15 22:57:42 +0900

    feat(API): api.js 생성 및 호출 테스트

---
+++ src/API/api.js
@@ -0,0 +1,18 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+export async function tmpAPI(formDate) {
+  console.log(API_KEY);
+  const response = await fetch(
+    `http://apis.data.go.kr/B551011/KorService/searchKeyword?serviceKey=${API_KEY}&_type=json&MobileOS=WIN&numOfRows=10&MobileApp=test&arrange=P&keyword=광화문`,
+    {
+      method: "GET",
+      body: formDate,
+    }
+  );
+  if (!response.ok) {
+    throw new Error("정보를 불러오는데 실패했습니다.");
+  }
+  const body = await response.json();
+  console.log(body);
+  return body;
+}
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import { tmpAPI } from "../API/api";
+
+const Main = () => {
+  console.log(tmpAPI());
+  return (
+    <div>
+      <h1>123</h1>
+    </div>
+  );
+};
+
+export default Main;
