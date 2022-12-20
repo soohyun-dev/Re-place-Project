1f45e6c02b4dc8000f7b75150051441f045eed93
soohyun <bmy1320@gmail.com>
2022-12-20 19:24:05 +0900

    feat(RegionContent): 지역별 명소 출력 기능 추가

--- src/API/api.js
+++ src/API/api.js
@@ -16,18 +16,17 @@ export async function regionAPI(formDate) {
   return body.response.body.items.item;
 }
 
-export async function regionsAPI(formDate) {
+export async function regionsAPI(regionCode) {
   const response = await fetch(
-    `http://apis.data.go.kr/B551011/KorService/areaBasedList?serviceKey=${API_KEY}&pageNo=1&numOfRows=500&MobileApp=AppTest&MobileOS=ETC&arrange=A&areaCode=1&_type=json`,
+    `http://apis.data.go.kr/B551011/KorService/areaBasedList?serviceKey=${API_KEY}&pageNo=1&numOfRows=20&MobileApp=AppTest&MobileOS=ETC&arrange=A&areaCode=${regionCode}&_type=json`,
     {
       method: "GET",
-      body: formDate,
     }
   );
   if (!response.ok) {
     throw new Error("정보를 불러오는데 실패했습니다.");
   }
   const body = await response.json();
-  console.log(body);
-  return body;
+  console.log(body.response.body.items.item);
+  return body.response.body.items.item;
 }
--- src/components/RegionContent.js
+++ src/components/RegionContent.js
@@ -1,16 +1,32 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { regionsAPI } from "../API/api";
 import Nav from "./Nav";
 
 const RegionContent = () => {
+  const [place, setPlace] = useState([]);
   const location = useLocation();
   const code = location.state.code;
-  const name = location.state.name;
-  console.log(code, name);
+
+  const handleAPI = async () => {
+    const result = await regionsAPI(code);
+    setPlace(
+      result.map((region) => {
+        return [region["addr1"], region["title"]];
+      })
+    );
+  };
+
+  useEffect(() => {
+    handleAPI();
+  }, []);
 
   return (
     <>
       <Nav />
-      <div>{(code, name)}</div>
+      {place.map((PLACE) => {
+        return <div>{PLACE}</div>;
+      })}
     </>
   );
 };
