b08a812323438d35807f4529c519a8f9aeee0fd7
soohyun <bmy1320@gmail.com>
2022-12-21 15:57:26 +0900

    feat: logo img 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,17 @@
 import "./Nav.css";
+import logo from "../img/logo.jpg";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <>
       <div className="nav">
-        <div className="logo"></div>
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} className="logo" alt="logo" />
+          </Link>
+        </div>
+        <div></div>
         <div></div>
       </div>
     </>
